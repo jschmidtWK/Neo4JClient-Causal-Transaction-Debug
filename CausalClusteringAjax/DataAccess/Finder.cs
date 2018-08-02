b9b194c1d3de20a1d4425645665f9ac043eb1f28
Jonathan Schmidt <jonathan@waykonect.com>
2018-08-02 12:33:00 +0200

    Added Finder class to ressemble original project.

---
+++ CausalClusteringAjax/DataAccess/Finder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Library.Data;
+
+namespace Library
+{
+    public static class Finder
+    {
+        public static Neo4jClient.Transactions.ITransaction BeginNeo4JTransaction(Neo4jClient.Transactions.TransactionScopeOption option = Neo4jClient.Transactions.TransactionScopeOption.Join) => NeoStore.BeginTransaction(option);
+    }
+}
